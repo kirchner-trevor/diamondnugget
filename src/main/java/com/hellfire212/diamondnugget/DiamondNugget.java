ca5a2a3b1b930779d6c267da269f8083baaf6001
kirchner-trevor <kirchner.trevor@gmail.com>
2018-11-24 01:53:22 -0600

    Added diamond armor and tool smelting recipes.

--- src/main/java/com/hellfire212/diamondnugget/DiamondNugget.java
+++ src/main/java/com/hellfire212/diamondnugget/DiamondNugget.java
@@ -2,11 +2,15 @@ package com.hellfire212.diamondnugget;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -21,6 +25,20 @@ public class DiamondNugget {
                 .setUnlocalizedName("diamond_nugget")
                 .setCreativeTab(CreativeTabs.MISC)
                 .setMaxStackSize(64);
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event) {
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_SWORD, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_PICKAXE, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_AXE, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_SHOVEL, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HOE, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HELMET, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_CHESTPLATE, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_LEGGINGS, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_BOOTS, new ItemStack(DIAMOND_NUGGET), 0.1f);
+        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HORSE_ARMOR, new ItemStack(DIAMOND_NUGGET), 0.1f);
     }
 
     @SubscribeEvent
