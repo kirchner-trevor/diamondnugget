0b01bd2168683971b6dffb0fe3cd778cd76e5ad0
Trevor <kirchner.trevor@gmail.com>
2022-07-11 09:23:13 -0500

    Updated for MC 1.18.2

--- src/main/java/com/hellfire212/diamondnugget/DiamondNugget.java
+++ src/main/java/com/hellfire212/diamondnugget/DiamondNugget.java
@@ -1,54 +1,31 @@
 package com.hellfire212.diamondnugget;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-@Mod(modid = "diamondnugget", name = "Diamond Nugget", version = "0.0.0-0.0.0.0")
-@Mod.EventBusSubscriber
-public class DiamondNugget {
-    private static Item DIAMOND_NUGGET;
+@Mod(DiamondNugget.MODID)
+@Mod.EventBusSubscriber(modid = DiamondNugget.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DiamondNugget
+{
+    public static final String MODID = "diamondnugget";
 
-    static {
-        DIAMOND_NUGGET = new Item()
-                .setRegistryName("diamond_nugget")
-                .setUnlocalizedName("diamond_nugget")
-                .setCreativeTab(CreativeTabs.MISC)
-                .setMaxStackSize(64);
+    public DiamondNugget() {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ITEMS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @Mod.EventHandler
-    public static void init(FMLInitializationEvent event) {
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_SWORD, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_PICKAXE, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_AXE, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_SHOVEL, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HOE, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HELMET, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_CHESTPLATE, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_LEGGINGS, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_BOOTS, new ItemStack(DIAMOND_NUGGET), 0.1f);
-        FurnaceRecipes.instance().addSmelting(Items.DIAMOND_HORSE_ARMOR, new ItemStack(DIAMOND_NUGGET), 0.1f);
-    }
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DiamondNugget.MODID);
 
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().register(DIAMOND_NUGGET);
-        OreDictionary.registerOre("nuggetDiamond", DIAMOND_NUGGET);
-    }
-
-    @SubscribeEvent
-    public static void registerRenders(ModelRegistryEvent event) {
-        ModelLoader.setCustomModelResourceLocation(DIAMOND_NUGGET, 0, new ModelResourceLocation(DIAMOND_NUGGET.getRegistryName(), "inventory"));
-    }
+    public static final RegistryObject<Item> DIAMOND_NUGGET = ITEMS.register(
+            "diamond_nugget", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 }
